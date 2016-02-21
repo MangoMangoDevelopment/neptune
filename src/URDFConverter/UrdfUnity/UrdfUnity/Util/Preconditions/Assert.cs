97f756b5379428170f29992b791687b5dd2b0425
Amber-Lynn Wareham <a.wareham@gmail.com>
2016-02-21 13:28:05 -0500

    Created utility class to validate parameters that can't be null

---
+++ src/URDFConverter/UrdfUnity/UrdfUnity/Util/Preconditions/Assert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrdfUnity.Util.Preconditions
+{
+    /// <summary>
+    /// Utility class for parameter validation.
+    /// </summary>
+    public static class Assert
+    {
+        /// <summary>
+        /// Asserts that the object is not null, throwing an <c>ArgumentNullException</c> with the provided message otherwise.
+        /// </summary>
+        /// <param name="obj">The object being validated as not null</param>
+        /// <param name="paramName">The name of the parameter being validated</param>
+        public static void IsNotNull(Object obj, String paramName)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+    }
+}
---
+++ src/URDFConverter/UrdfUnity/UrdfUnityTest/Util/Preconditions/AssertTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UrdfUnityTest.Preconditions.Util
+{
+    [TestClass]
+    public class AssertTest
+    {
+        [TestMethod]
+        public void IsNotNullTrue()
+        {
+            String str = "Oh, hello";
+            UrdfUnity.Util.Preconditions.Assert.IsNotNull(str, "str");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void IsNotNullFalse()
+        {
+            String str = null;
+            UrdfUnity.Util.Preconditions.Assert.IsNotNull(str, "str");
+        }
+    }
+}
