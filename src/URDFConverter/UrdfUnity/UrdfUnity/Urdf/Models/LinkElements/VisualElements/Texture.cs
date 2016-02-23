28b17af83f5de49a9419366b81028668513f6838
Amber-Lynn Wareham <a.wareham@gmail.com>
2016-02-23 14:19:33 -0500

    Created model class for the URDF texture element

---
+++ src/URDFConverter/UrdfUnity/UrdfUnity/Urdf/Models/LinkElements/VisualElements/Texture.cs
@@ -0,0 +1,26 @@
+﻿using UrdfUnity.Util;
+
+namespace UrdfUnity.Urdf.Models.LinkElements.VisualElements
+{
+    /// <summary>
+    /// Represents the texture of a material of a visual element's material.
+    /// </summary>
+    public class Texture
+    {
+        /// <summary>
+        /// The file name of the texture for the visual element.
+        /// </summary>
+        public string FileName { get; }
+
+
+        /// <summary>
+        /// Creates a new instance of Texture.
+        /// </summary>
+        /// <param name="fileName">The file name of the texture</param>
+        public Texture(string fileName)
+        {
+            Preconditions.IsNotEmpty(fileName);
+            this.FileName = fileName;
+        }
+    }
+}
---
+++ src/URDFConverter/UrdfUnity/UrdfUnityTest/Urdf/Models/LinkElements/VisualElements/TextureTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrdfUnity.Urdf.Models.LinkElements.VisualElements;
+
+namespace UrdfUnityTest.Urdf.Models.LinkElements.VisualElements
+{
+    [TestClass]
+    public class TextureTest
+    {
+        [TestMethod]
+        public void ConstructTexture()
+        {
+            string fileName = "fileName";
+            Texture texture = new Texture(fileName);
+
+            Assert.AreEqual(fileName, texture.FileName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructTextureNoFileName()
+        {
+            Texture texture = new Texture("");
+        }
+    }
+}
