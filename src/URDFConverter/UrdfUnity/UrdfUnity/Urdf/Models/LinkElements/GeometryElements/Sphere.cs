7e25e266b3dd48f5e82e32bf0545c643e3276ea4
Amber-Lynn Wareham <a.wareham@gmail.com>
2016-02-23 10:50:21 -0500

    Created model class for the URDF sphere element

---
+++ src/URDFConverter/UrdfUnity/UrdfUnity/Urdf/Models/LinkElements/GeometryElements/Sphere.cs
@@ -0,0 +1,23 @@
+﻿namespace UrdfUnity.Urdf.Models.LinkElements.GeometryElements
+{
+    /// <summary>
+    /// Represents the sphere shape of a link's geometry.
+    /// </summary>
+    public class Sphere
+    {
+        /// <summary>
+        /// The radius of the sphere shape.
+        /// </summary>
+        public double Radius { get; }
+
+
+        /// <summary>
+        /// Creates a new instance of Sphere.
+        /// </summary>
+        /// <param name="radius">The radius of the sphere</param>
+        public Sphere(double radius)
+        {
+            this.Radius = radius;
+        }
+    }
+}
---
+++ src/URDFConverter/UrdfUnity/UrdfUnityTest/Urdf/Models/LinkElements/GeometryElements/SphereTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrdfUnity.Urdf.Models.LinkElements.GeometryElements;
+
+namespace UrdfUnityTest.Urdf.Models.LinkElements.GeometryElements
+{
+    [TestClass]
+    class SphereTest
+    {
+        [TestMethod]
+        public void ConstructSphere()
+        {
+            double radius = 1;
+            Sphere sphere = new Sphere(radius);
+
+            Assert.AreEqual(radius, sphere.Radius);
+        }
+    }
+}
