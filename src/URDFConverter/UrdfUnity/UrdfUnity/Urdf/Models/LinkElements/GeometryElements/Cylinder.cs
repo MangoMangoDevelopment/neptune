805def3e8b0a9ddf85aa9750605290159016d180
Amber-Lynn Wareham <a.wareham@gmail.com>
2016-02-23 10:42:29 -0500

    Created model class for URDF cylinder element

---
+++ src/URDFConverter/UrdfUnity/UrdfUnity/Urdf/Models/LinkElements/GeometryElements/Cylinder.cs
@@ -0,0 +1,30 @@
+﻿namespace UrdfUnity.Urdf.Models.LinkElements.GeometryElements
+{
+    /// <summary>
+    /// Represents the cylinder shape of a link's geometry.
+    /// </summary>
+    public class Cylinder
+    {
+        /// <summary>
+        /// The radius of the cylinder shape.
+        /// </summary>
+        public double Radius { get; }
+
+        /// <summary>
+        /// The length of the cylinder shape.
+        /// </summary>
+        public double Length { get; }
+
+
+        /// <summary>
+        /// Creates a new instance of Cylinder.
+        /// </summary>
+        /// <param name="radius">The radius of the cylinder</param>
+        /// <param name="length">The length of the cylinder</param>
+        public Cylinder(double radius, double length)
+        {
+            this.Radius = radius;
+            this.Length = length;
+        }
+    }
+}
---
+++ src/URDFConverter/UrdfUnity/UrdfUnityTest/Urdf/Models/LinkElements/GeometryElements/CylinderTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrdfUnity.Urdf.Models.LinkElements.GeometryElements;
+
+namespace UrdfUnityTest.Urdf.Models.LinkElements.GeometryElements
+{
+    [TestClass]
+    public class CylinderTest
+    {
+        [TestMethod]
+        public void ConstructCylinder()
+        {
+            double radius = 1;
+            double length = 2;
+            Cylinder cylinder = new Cylinder(radius, length);
+
+            Assert.AreEqual(radius, cylinder.Radius);
+            Assert.AreEqual(length, cylinder.Length);
+        }
+    }
+}
