50209a2a5b1e684cd83690af3004ee1bedcc30a6
Amber-Lynn Wareham <a.wareham@gmail.com>
2016-02-23 14:40:15 -0500

    Created model class for the URDF material element

---
+++ src/URDFConverter/UrdfUnity/UrdfUnity/Urdf/Models/LinkElements/VisualElements/Material.cs
@@ -0,0 +1,72 @@
+﻿using UrdfUnity.Util;
+
+namespace UrdfUnity.Urdf.Models.LinkElements.VisualElements
+{
+    /// <summary>
+    /// Represents the material of a visual element, including its colour and texture.
+    /// </summary>
+    public class Material
+    {
+        /// <summary>
+        /// The name identifier of the material.
+        /// </summary>
+        /// <value>Required.</value>
+        public string Name { get; }
+
+        /// <summary>
+        /// The color specification of the material.
+        /// </summary>
+        /// <value>Optional. MAY BE NULL.</value>
+        public Color Color { get; }
+
+        /// <summary>
+        /// The texture of the material.
+        /// </summary>
+        /// <value>Optional. MAY BE NULL.</value>
+        public Texture Texture { get; }
+
+
+        /// <summary>
+        /// Creates a new instance of Material with the specified name.
+        /// </summary>
+        /// <param name="name">The name identifier of the material</param>
+        public Material(string name) : this(name, null, null)
+        {
+            // Invoke overloaded constructor.
+        }
+
+        /// <summary>
+        /// Creates a new instance of Material with the specified name and colour.
+        /// </summary>
+        /// <param name="name">The name identifier of the material</param>
+        /// <param name="color">The colour of the materialL</param>
+        public Material(string name, Color color) : this(name, color, null)
+        {
+            // Invoke overloaded constructor.
+        }
+
+        /// <summary>
+        /// Creates a new instance of Material with the specified name and texture.
+        /// </summary>
+        /// <param name="name">The name identifier of the material</param>
+        /// <param name="texture">The texture of the material</param>
+        public Material(string name, Texture texture) : this(name, null, texture)
+        {
+            // Invoke overloaded constructor.
+        }
+
+        /// <summary>
+        /// Creates a new instance of Material with the specified name, colour and texture.
+        /// </summary>
+        /// <param name="name">The name identifier of the material</param>
+        /// <param name="color">The colour of the material. MAY BE NULL</param>
+        /// <param name="texture">The texture of the material. MAY BE NULL</param>
+        public Material(string name, Color color, Texture texture)
+        {
+            Preconditions.IsNotEmpty(name);
+            this.Name = name;
+            this.Color = color;
+            this.Texture = texture;
+        }
+    }
+}
---
+++ src/URDFConverter/UrdfUnity/UrdfUnityTest/Urdf/Models/LinkElements/VisualElements/MaterialTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrdfUnity.Urdf.Models.LinkElements.VisualElements;
+
+namespace UrdfUnityTest.Urdf.Models.LinkElements.VisualElements
+{
+    [TestClass]
+    public class MaterialTest
+    {
+        [TestMethod]
+        public void ConstructMaterial()
+        {
+            string name = "name";
+            Color color = new Color(new RgbAttribute(0, 0, 0));
+            Texture texture = new Texture("fileName");
+            Material material = new Material(name, color, texture);
+
+            Assert.AreEqual(name, material.Name);
+            Assert.AreEqual(color, material.Color);
+            Assert.AreEqual(texture, material.Texture);
+        }
+
+        [TestMethod]
+        public void ConstructMaterialNameOnly()
+        {
+            string name = "name";
+            Material material = new Material(name);
+
+            Assert.AreEqual(name, material.Name);
+            Assert.IsNull(material.Color);
+            Assert.IsNull(material.Texture);
+        }
+
+        [TestMethod]
+        public void ConstructMaterialWithColor()
+        {
+            string name = "name";
+            Color color = new Color(new RgbAttribute(0, 0, 0));
+            Material material = new Material(name, color);
+
+            Assert.AreEqual(name, material.Name);
+            Assert.AreEqual(color, material.Color);
+            Assert.IsNull(material.Texture);
+        }
+
+        [TestMethod]
+        public void ConstructMaterialWithTexture()
+        {
+            string name = "name";
+            Texture texture = new Texture("fileName");
+            Material material = new Material(name, texture);
+
+            Assert.AreEqual(name, material.Name);
+            Assert.IsNull(material.Color);
+            Assert.AreEqual(texture, material.Texture);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ContructMaterialNoName()
+        {
+            Material material = new Material("");
+        }
+    }
+}
