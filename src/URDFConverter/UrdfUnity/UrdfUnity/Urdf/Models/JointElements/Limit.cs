91826009af061a9efe5731952ee28a3916c9ba2d
Amber-Lynn Wareham <a.wareham@gmail.com>
2016-02-24 09:38:11 -0500

    Created model class for URDF limit element

---
+++ src/URDFConverter/UrdfUnity/UrdfUnity/Urdf/Models/JointElements/Limit.cs
@@ -0,0 +1,63 @@
+﻿namespace UrdfUnity.Urdf.Models.JointElements
+{
+    /// <summary>
+    /// Represents the physical limitations of the movement of a joint.
+    /// </summary>
+    /// <remarks>
+    /// The limit is required only for revolute and prismatic joints.
+    /// </remarks>
+    public class Limit
+    {
+        private static readonly double DEFAULT_VALUE = 0d;
+
+        /// <summary>
+        /// The lower joint limit.
+        /// </summary>
+        /// <value>Optional. In radians for revolute joints; In meters for prismatic joints.</value>
+        public double Lower { get; }
+
+        /// <summary>
+        /// The upper joint limit.
+        /// </summary>
+        /// <value>Optional. In radians for revolute joints; In meters for prismatic joints.</value>
+        public double Upper { get; }
+
+        /// <summary>
+        /// The maximum joint effort that should be enforced.
+        /// </summary>
+        /// <value>Required. |applied effort| &lt; |effort|</value>
+        public double Effort { get; }
+
+        /// <summary>
+        /// The maximum joint velocity that should be enforced.
+        /// </summary>
+        /// <value>Required.</value>
+        public double Velocity { get; }
+
+
+        /// <summary>
+        /// Creates a new instance of Limit.
+        /// </summary>
+        /// <param name="effort">The maximum joint effort</param>
+        /// <param name="velocity">The maximum joint velocity</param>
+        public Limit(double effort, double velocity) : this(DEFAULT_VALUE, DEFAULT_VALUE, effort, velocity)
+        {
+            // Invoke overloaded constructor.
+        }
+        
+        /// <summary>
+        /// Creates a new instance of Limit.
+        /// </summary>
+        /// <param name="lower">The lower joint limit</param>
+        /// <param name="upper">The upper joint limit</param>
+        /// <param name="effort">The maximum joint effort</param>
+        /// <param name="velocity">The maximum joint velocity</param>
+        public Limit(double lower, double upper, double effort, double velocity)
+        {
+            this.Lower = lower;
+            this.Upper = upper;
+            this.Effort = effort;
+            this.Velocity = velocity;
+        }
+    }
+}
---
+++ src/URDFConverter/UrdfUnity/UrdfUnityTest/Urdf/Models/JointElements/LimitTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrdfUnity.Urdf.Models.JointElements;
+
+namespace UrdfUnityTest.Urdf.Models.JointElements
+{
+    [TestClass]
+    public class LimitTest
+    {
+        [TestMethod]
+        public void ConstructLimit()
+        {
+            double lower = 1;
+            double upper = 2;
+            double effort = 3;
+            double velocity = 4;
+            Limit limit = new Limit(lower, upper, effort, velocity);
+
+            Assert.AreEqual(lower, limit.Lower);
+            Assert.AreEqual(upper, limit.Upper);
+            Assert.AreEqual(effort, limit.Effort);
+            Assert.AreEqual(velocity, limit.Velocity);
+        }
+
+        [TestMethod]
+        public void ConstructLimitNoLowerUpper()
+        {
+            double effort = 1;
+            double velocity = 2;
+            Limit limit = new Limit(effort, velocity);
+
+            Assert.AreEqual(0, limit.Lower);
+            Assert.AreEqual(0, limit.Upper);
+            Assert.AreEqual(effort, limit.Effort);
+            Assert.AreEqual(velocity, limit.Velocity);
+        }
+    }
+}
