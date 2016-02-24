9968e91d2ff0cc7b3e15df2e72d35482155e1c9b
Amber-Lynn Wareham <a.wareham@gmail.com>
2016-02-24 09:24:26 -0500

    Created model class for URDF dynamics element

---
+++ src/URDFConverter/UrdfUnity/UrdfUnity/Urdf/Models/JointElements/Dynamics.cs
@@ -0,0 +1,33 @@
+﻿namespace UrdfUnity.Urdf.Models.JointElements
+{
+    /// <summary>
+    /// Represents the physical properties used to specify modeling properties of the joint.
+    /// </summary>
+    /// <seealso cref="http://wiki.ros.org/urdf/XML/joint"/>
+    public class Dynamics
+    {
+        /// <summary>
+        /// The physical damping value of the joint.
+        /// </summary>
+        /// <value>Optional. (N*s/m) for prismatic joints; (N*m*s/rad) for revolute joints.</value>
+        public double Damping { get; }
+
+        /// <summary>
+        /// The physical static friction value of the joint.
+        /// </summary>
+        /// <value>Optional. (N) for prismatic joints; (N*m) for revolute joints.</value>
+        public double Friction { get; }
+
+
+        /// <summary>
+        /// Creates a new instance of Dynamics.
+        /// </summary>
+        /// <param name="damping">The physical damping value of the joint. Default value should be 0</param>
+        /// <param name="friction">The physical static friction value of the joint. Default value should be 0</param>
+        public Dynamics(double damping, double friction)
+        {
+            this.Damping = damping;
+            this.Friction = friction;
+        }
+    }
+}
---
+++ src/URDFConverter/UrdfUnity/UrdfUnityTest/Urdf/Models/JointElements/DynamicsTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrdfUnity.Urdf.Models.JointElements;
+
+namespace UrdfUnityTest.Urdf.Models.JointElements
+{
+    [TestClass]
+    public class DynamicsTest
+    {
+        [TestMethod]
+        public void ConstructDynamics()
+        {
+            double damping = 1;
+            double friction = 2;
+            Dynamics dynamics = new Dynamics(damping, friction);
+
+            Assert.AreEqual(damping, dynamics.Damping);
+            Assert.AreEqual(friction, dynamics.Friction);
+        }
+    }
+}
