03537e7f677d6c9591c2a7dd951dd7f925044724
Amber-Lynn Wareham <a.wareham@gmail.com>
2016-02-29 15:08:16 -0500

    Created interfaces for parsing URDF XML

---
+++ src/URDFConverter/UrdfUnity/UrdfUnity/Parse/Parser.cs
@@ -0,0 +1,17 @@
+﻿namespace UrdfUnity.Parse
+{
+    /// <summary>
+    /// Defines the generic interface implemented by utility parsing classes.
+    /// </summary>
+    /// <typeparam name="T">The object type being parsed and returned</typeparam>
+    /// <typeparam name="E">The type of input element being parsed from</typeparam>
+    public interface Parser<T, E>
+    {
+        /// <summary>
+        /// Parses the provided input and returns the parsed object.
+        /// </summary>
+        /// <param name="input">The input that will be parsed</param>
+        /// <returns>The object that was parsed from the input element</returns>
+        T Parse(E input);
+    }
+}
---
+++ src/URDFConverter/UrdfUnity/UrdfUnity/Parse/XmlParser.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml;
+
+namespace UrdfUnity.Parse
+{
+    /// <summary>
+    /// Defines the interface implemented by classes parsing from XML.
+    /// </summary>
+    /// <typeparam name="T">The object type being parsed and returned</typeparam>
+    /// <seealso cref="Parser{T, E}"/>
+    interface XmlParser<T> : Parser<T, XmlNode>
+    {
+    }
+}
