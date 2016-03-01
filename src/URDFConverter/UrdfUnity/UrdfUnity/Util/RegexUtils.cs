fa75c32b23a798a5b9ec198acac63ca572a20cc0
Amber-Lynn Wareham <a.wareham@gmail.com>
2016-03-01 14:24:23 -0500

    Created RegexUtils class to help with parsing URDF

---
+++ src/URDFConverter/UrdfUnity/UrdfUnity/Util/RegexUtils.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UrdfUnity.Util
+{
+    /// <summary>
+    /// Utility class for using Regex patterns for parsing values from strings.
+    /// </summary>
+    static public class RegexUtils
+    {
+        /// <summary>
+        /// The Regex pattern for matching real numbers (such as <c>float</c>s and <c>double</c>s) from a string.
+        /// </summary>
+        public static readonly string REAL_NUMBER_PATTERN = @"-?\d+\.?\d*";
+
+        /// <summary>
+        /// A Regex object used for matching real numbers as per <c>REAL_NUMBER_PATTERN</c>.
+        /// </summary>
+        private static readonly Regex REAL_NUMBER_REGEX = new Regex(REAL_NUMBER_PATTERN);
+
+
+        /// <summary>
+        /// Converts the first instance of a real number value in a string into a <c>double</c>.
+        /// </summary>
+        /// <param name="input">The string being matched as a double value. MUST BE A VALID MATCH</param>
+        /// <returns>The double value found in the input string</returns>
+        public static double MatchDouble(string input)
+        {
+            Preconditions.IsTrue(REAL_NUMBER_REGEX.IsMatch(input));
+
+            Match match = REAL_NUMBER_REGEX.Match(input);
+            return Double.Parse(match.Value);
+        }
+
+        /// <summary>
+        /// Converts all instances of a real number value in a string into a <c>double</c> array.
+        /// </summary>
+        /// <param name="input">The string being matched as a double values</param>
+        /// <returns>An array of double values found in the input string</returns>
+        public static double[] MatchDoubles(string input)
+        {
+            MatchCollection matches = REAL_NUMBER_REGEX.Matches(input);
+            double[] doubles = new double[matches.Count];
+
+            for(int i = 0; i < matches.Count; i++)
+            {
+                doubles[i] = Double.Parse(matches[i].Value);
+            }
+
+            return doubles;
+        }
+    }
+}
---
+++ src/URDFConverter/UrdfUnity/UrdfUnityTest/Util/RegexUtilsTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrdfUnity.Util;
+
+namespace UrdfUnityTest.Util
+{
+    [TestClass]
+    public class RegexUtilsTest
+    {
+        [TestMethod]
+        public void RealNumberPattern()
+        {
+            Assert.AreEqual(@"-?\d+\.?\d*", RegexUtils.REAL_NUMBER_PATTERN);
+        }
+
+        [TestMethod]
+        public void MatchDouble()
+        {
+            Assert.AreEqual(3.141592d, RegexUtils.MatchDouble("3.141592"));
+            Assert.AreEqual(0d, RegexUtils.MatchDouble("0.000"));
+            Assert.AreEqual(2d, RegexUtils.MatchDouble("2"));
+            Assert.AreEqual(-12345.6789d, RegexUtils.MatchDouble("-12345.6789"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MatchDoubleNoMatch()
+        {
+            RegexUtils.MatchDouble("not a number");
+        }
+
+        [TestMethod]
+        public void MatchDoubles()
+        {
+            double pi = 3.14;
+            double tau = 6.28;
+            string numbersAsString = pi.ToString() + " " + tau.ToString();
+            double[] results = RegexUtils.MatchDoubles(numbersAsString);
+
+            Assert.IsTrue(results.Length == 2);
+            Assert.AreEqual(pi, results[0]);
+            Assert.AreEqual(tau, results[1]);
+        }
+
+        [TestMethod]
+        public void MatchDoublesNoMatch()
+        {
+            double[] result = RegexUtils.MatchDoubles("no numbers");
+
+            Assert.IsTrue(result.Length == 0);
+        }
+    }
+}
