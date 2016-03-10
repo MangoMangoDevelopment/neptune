55c6b60d2b0a962172821939a08001516407696f
Amber-Lynn Wareham <a.wareham@gmail.com>
2016-03-10 12:07:04 -0500

    Implemented XML parser for URDF <geometry> element

--- src/URDFConverter/UrdfUnity/UrdfUnity/Parse/Xml/LinkElements/GeometryParser.cs
+++ src/URDFConverter/UrdfUnity/UrdfUnity/Parse/Xml/LinkElements/GeometryParser.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Xml;
+using UrdfUnity.Parse.Xml.LinkElements.GeometryElements;
 using UrdfUnity.Urdf.Models.LinkElements;
+using UrdfUnity.Urdf.Models.LinkElements.GeometryElements;
+using UrdfUnity.Util;
 
 namespace UrdfUnity.Parse.Xml.LinkElements
 {
@@ -10,13 +13,18 @@ namespace UrdfUnity.Parse.Xml.LinkElements
     /// <seealso cref="http://wiki.ros.org/urdf/XML/link"/>
     /// <seealso cref="http://wiki.ros.org/urdf/XML/visual"/>
     /// <seealso cref="Urdf.Models.LinkElements.Geometry"/>
-    class GeometryParser : XmlParser<Geometry>
+    public class GeometryParser : XmlParser<Geometry>
     {
-        private static readonly string BOX_ATTRIBUTE_NAME = "box";
-        private static readonly string CYLINDER_ATTRIBUTE_NAME = "cylinder";
-        private static readonly string SPHERE_ATTRIBUTE_NAME = "sphere";
-        private static readonly string MESH_ATTRIBUTE_NAME = "mesh";
-        private static readonly string MATERIAL_ATTRIBUTE_NAME = "material";
+        private static readonly string BOX_ELEMENT_NAME = "box";
+        private static readonly string CYLINDER_ELEMENT_NAME = "cylinder";
+        private static readonly string SPHERE_ELEMENT_NAME = "sphere";
+        private static readonly string MESH_ELEMENT_NAME = "mesh";
+        private static readonly Geometry DEFAULT_GEOMETRY = new Geometry(new Box(new SizeAttribute(1, 1, 1)));
+
+        private readonly BoxParser boxParser = new BoxParser();
+        private readonly CylinderParser cylinderParser = new CylinderParser();
+        private readonly SphereParser sphereParser = new SphereParser();
+        private readonly MeshParser meshParser = new MeshParser();
 
 
         /// <summary>
@@ -26,8 +34,38 @@ namespace UrdfUnity.Parse.Xml.LinkElements
         /// <returns>A Geometry object parsed from the XML</returns>
         public Geometry Parse(XmlNode node)
         {
-            // TODO: Implement...!
-            throw new NotImplementedException();
+            Preconditions.IsNotNull(node);
+
+            XmlElement boxElement = (XmlElement)node.SelectSingleNode(BOX_ELEMENT_NAME);
+            XmlElement cylinderElement = (XmlElement)node.SelectSingleNode(CYLINDER_ELEMENT_NAME);
+            XmlElement sphereElement = (XmlElement)node.SelectSingleNode(SPHERE_ELEMENT_NAME);
+            XmlElement meshElement = (XmlElement)node.SelectSingleNode(MESH_ELEMENT_NAME);
+
+            Geometry geometry = null;
+
+            if (boxElement != null)
+            {
+                geometry = new Geometry(this.boxParser.Parse(boxElement));
+            }
+            else if (cylinderElement != null)
+            {
+                geometry = new Geometry(this.cylinderParser.Parse(cylinderElement));
+            }
+            else if (sphereElement != null)
+            {
+                geometry = new Geometry(this.sphereParser.Parse(sphereElement));
+            }
+            else if (meshElement != null)
+            {
+                geometry = new Geometry(this.meshParser.Parse(meshElement));
+            }
+            else
+            {
+                // TODO: Log malformed <geometry> element encountered
+                geometry = DEFAULT_GEOMETRY;
+            }
+
+            return geometry;
         }
     }
 }
---
+++ src/URDFConverter/UrdfUnity/UrdfUnityTest/Parse/Xml/LinkElements/GeometryParserTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrdfUnity.Parse.Xml.LinkElements;
+using UrdfUnity.Urdf.Models.LinkElements;
+using UrdfUnity.Urdf.Models.LinkElements.GeometryElements;
+
+namespace UrdfUnityTest.Parse.Xml.LinkElements
+{
+    [TestClass]
+    public class GeometryParserTest
+    {
+        private readonly GeometryParser parser = new GeometryParser();
+        private readonly XmlDocument xmlDoc = new XmlDocument();
+
+
+        [TestMethod]
+        public void ParseGeometryBox()
+        {
+            Box box = new Box(new SizeAttribute(1, 2, 3));
+            string xml = String.Format("<geometry><box size='{0} {1} {2}'/></geometry>", box.Size.Length, box.Size.Width, box.Size.Height);
+
+            this.xmlDoc.Load(XmlReader.Create(new StringReader(xml)));
+            Geometry geometry = this.parser.Parse(this.xmlDoc.DocumentElement);
+
+            Assert.AreEqual(Geometry.Shapes.Box, geometry.Shape);
+            Assert.AreEqual(box, geometry.Box);
+            Assert.IsNull(geometry.Cylinder);
+            Assert.IsNull(geometry.Sphere);
+            Assert.IsNull(geometry.Mesh);
+        }
+
+        [TestMethod]
+        public void ParseGeometryCylinder()
+        {
+            Cylinder cylinder = new Cylinder(1, 2);
+            string xml = String.Format("<geometry><cylinder radius='{0}' length='{1}'/></geometry>", cylinder.Radius, cylinder.Length);
+
+            this.xmlDoc.Load(XmlReader.Create(new StringReader(xml)));
+            Geometry geometry = this.parser.Parse(this.xmlDoc.DocumentElement);
+
+            Assert.AreEqual(Geometry.Shapes.Cylinder, geometry.Shape);
+            Assert.AreEqual(cylinder, geometry.Cylinder);
+            Assert.IsNull(geometry.Box);
+            Assert.IsNull(geometry.Sphere);
+            Assert.IsNull(geometry.Mesh);
+        }
+
+        [TestMethod]
+        public void ParseGeometrySphere()
+        {
+            Sphere sphere = new Sphere(1);
+            string xml = String.Format("<geometry><sphere radius='{0}'/></geometry>", sphere.Radius);
+
+            this.xmlDoc.Load(XmlReader.Create(new StringReader(xml)));
+            Geometry geometry = this.parser.Parse(this.xmlDoc.DocumentElement);
+
+            Assert.AreEqual(Geometry.Shapes.Sphere, geometry.Shape);
+            Assert.AreEqual(sphere, geometry.Sphere);
+            Assert.IsNull(geometry.Box);
+            Assert.IsNull(geometry.Cylinder);
+            Assert.IsNull(geometry.Mesh);
+        }
+
+        [TestMethod]
+        public void ParseGeometryMesh()
+        {
+            Mesh mesh = new Mesh.Builder("filename").Build();
+            string xml = String.Format("<geometry><mesh filename='{0}'/></geometry>", mesh.FileName);
+
+            this.xmlDoc.Load(XmlReader.Create(new StringReader(xml)));
+            Geometry geometry = this.parser.Parse(this.xmlDoc.DocumentElement);
+
+            Assert.AreEqual(Geometry.Shapes.Mesh, geometry.Shape);
+            Assert.AreEqual(mesh, geometry.Mesh);
+            Assert.IsNull(geometry.Box);
+            Assert.IsNull(geometry.Cylinder);
+            Assert.IsNull(geometry.Sphere);
+        }
+
+        [TestMethod]
+        public void ParseGeometryMalformed()
+        {
+            Box defaultBox = new Box(new SizeAttribute(1, 1, 1));
+            string xml = "<geometry></geometry>"; // No shape
+
+            this.xmlDoc.Load(XmlReader.Create(new StringReader(xml)));
+            Geometry geometry = this.parser.Parse(this.xmlDoc.DocumentElement);
+
+            Assert.AreEqual(Geometry.Shapes.Box, geometry.Shape);
+            Assert.AreEqual(defaultBox, geometry.Box);
+            Assert.IsNull(geometry.Cylinder);
+            Assert.IsNull(geometry.Sphere);
+            Assert.IsNull(geometry.Mesh);
+        }
+    }
+}
