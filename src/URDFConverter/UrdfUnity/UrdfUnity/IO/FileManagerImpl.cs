d052262503f53faae626225b58c94bc92e399240
Brian Tran <tran.brian88@gmail.com>
2016-03-29 13:23:24 -0400

    Modified the files and class names to reflect correct meaning

--- src/URDFConverter/UrdfUnity/UrdfUnity/IO/FileManagerImpl.cs
+++ src/URDFConverter/UrdfUnity/UrdfUnity/IO/FileManagerImpl.cs
@@ -1,27 +1,109 @@
-﻿namespace UrdfUnity.IO
+﻿using System;
+using System.IO;
+using UrdfUnity.Urdf.Models;
+using UrdfUnity.Util;
+using UrdfUnity.Parse;
+using NLog;
+
+
+namespace UrdfUnity.IO
 {
     /// <summary>
-    /// Defines the generic interface implemented by utility parsing classes.
+    /// Handles file input/output for URDF and Xacro files
     /// </summary>
-    public interface FileManagerImpl
+    public class FileManagerImpl : FileManager
     {
-        /// <summary>
-        /// Reads a file given the provided path and returns the object.
-        /// </summary>
-        /// <param name="path">The path of file to be read</param>
-        /// <returns>The object that was found in the file</returns>
-        /// 
-        string ReadFileToString(string path);
-        
-    }
+        private static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
+        UrdfParser urdf = new UrdfParser();
 
-    /// <summary>
-    /// Filetypes we care about in the file manager.
-    /// </summary>
-    public enum FileType
-    {
-        UNKNOWN,
-        URDF,
-        XACRO
+
+        /// <summary>
+        /// Reads the entire file into a string.
+        /// </summary>
+        /// <param name="path">Path of file to read</param>
+        /// <returns>String representation of file, otherwise empty string.</returns>
+        public string ReadFileToString (string path)
+        {
+            string fileAsString = String.Empty;
+            try
+            {
+                using (StreamReader fileReader = new StreamReader(path))
+                {
+                    fileAsString = fileReader.ReadToEnd();
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                LOGGER.Warn("Error file not found \"{0}\"", path);
+            }
+            return fileAsString;
+        }
+
+
+        /// <summary>
+        /// Extracts a Robot model object from the provided URDF/Xacro filepath.
+        /// </summary>
+        /// <param name="filePath">Path of file containing urdf/xacro for the robot</param>
+        /// <returns>
+        ///     The Robot model parsed from the specified filepath if valid, otherwise <c>null<c>
+        /// </returns>
+        public Robot GetRobotFromFile(string filePath)
+        {
+            Robot robo = null;
+
+            string contents = ReadFileToString(filePath);
+
+            if (!contents.Equals(string.Empty))
+            {
+                FileType type = GetFileType(filePath);
+                switch(type)
+                {
+                    case FileType.URDF:
+                        robo = this.urdf.Parse(contents);
+                        break;
+                    case FileType.XACRO:
+                        // TODO: Call Xacro parser
+                        throw new NotImplementedException();
+                        break;
+                    default:
+                        LOGGER.Warn("Unknown filetype supplied. Unable to parse robot.");
+                        break;
+                }
+            }
+            else
+            {
+                LOGGER.Warn("Empty or no file loaded.");
+            }
+
+            return robo;
+        }
+
+
+        /// <summary>
+        /// Given a file path, this will extract the extention and return an
+        /// enum type of FileType indicating whether we're trying to parse a
+        /// URDF or XACRO file.
+        /// </summary>
+        /// <param name="file">This can be a string representation of the file path or just filename</param>
+        /// <returns></returns>
+        public FileType GetFileType(string fileName)
+        {
+            // remove the . from the get extension function, so that it can be properly translated to an enum type of FileType
+            string extension = Path.GetExtension(fileName).Replace('.', '\0');
+            FileType type = FileType.UNKNOWN;
+            try
+            {
+                type = EnumUtils.ToEnum<FileType>(extension);
+            }
+            catch (OverflowException e)
+            {
+                LOGGER.Warn("File type has not been defined.");
+            }
+            catch (ArgumentException e)
+            {
+                LOGGER.Warn("File type contains white space or is null.");
+            }
+            return type;
+        }
     }
 }
