a6f234e216e8f26148262d5e0dc888c8bcbb0060
Amber-Lynn Wareham <a.wareham@gmail.com>
2016-04-17 15:11:11 -0400

    Created utility class XmlStringBuilder

---
+++ src/URDFConverter/UrdfUnity/UrdfUnityTest/Util/XmlStringBuilderTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrdfUnity.Util;
+
+namespace UrdfUnityTest.Util
+{
+    [TestClass]
+    public class XmlStringBuilderTest
+    {
+        [TestMethod]
+        public void BuildXmlString()
+        {
+            string expected = "<hello a=\"b\">\r\n<subelement/>\r\n</hello>";
+            XmlStringBuilder sb = new XmlStringBuilder("hello");
+
+            sb.AddAttribute("a", "b").AddSubElement("<subelement/>");
+
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+        [TestMethod]
+        public void BuildXmlStringAttributesOnly()
+        {
+            string expected = "<hello a=\"b\" c=\"4\"/>";
+            XmlStringBuilder sb = new XmlStringBuilder("hello");
+
+            sb.AddAttribute("a", "b").AddAttribute("c", 4);
+
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+        [TestMethod]
+        public void BuildXmlStringSubElementsOnly()
+        {
+            string expected = "<hello>\r\n<one/>\r\n<two/>\r\n</hello>";
+            XmlStringBuilder sb = new XmlStringBuilder("hello");
+
+            sb.AddSubElement("<one/>").AddSubElement("<two/>");
+
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+        [TestMethod]
+        public void BuildXmlStringNoAttributesOrSubElements()
+        {
+            string expected = "<hello/>";
+            XmlStringBuilder sb = new XmlStringBuilder("hello");
+
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+        [TestMethod]
+        public void ResetXmlString()
+        {
+            string expected = "<good bye=\"bye\"/>";
+            XmlStringBuilder sb = new XmlStringBuilder("hello");
+
+            sb.Reset("good").AddAttribute("bye", "bye");
+
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void BuildXmlStringNoElement()
+        {
+            new XmlStringBuilder("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void BuildXmlStringNullElement()
+        {
+            new XmlStringBuilder(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ResetXmlStringNoElement()
+        {
+            new XmlStringBuilder("hello").Reset("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ResetXmlStringNullElement()
+        {
+            new XmlStringBuilder("hello").Reset(null);
+        }
+    }
+}
